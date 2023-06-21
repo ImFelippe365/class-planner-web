6f2a3aac1953a172d0c9d078489938bf88a84e73
Felippe <felipperian365@gmail.com>
2023-06-21 14:18:38 -0300

    Criada página de login

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import Input from "../../components/Input";
+import { useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useRouter } from "next/navigation";
+
+interface ISignIn {
+	registration: string;
+	password: string;
+}
+
+export default function SignIn() {
+	const signInSchema = yup.object({
+		registration: yup.string().required("Campo e-mail é obrigatório"),
+		password: yup.string().required("Campo senha é obrigatório"),
+	});
+
+	const {
+		control,
+		handleSubmit,
+		formState: { isSubmitting },
+	} = useForm<ISignIn>({
+		resolver: yupResolver(signInSchema),
+	});
+
+	const router = useRouter();
+
+	const onSubmit = async (data: ISignIn) => {
+		console.log(data);
+
+		router.push('/courses')
+	};
+
+	return (
+		<div className="grid grid-cols-2 ">
+			<section className="m-auto">
+				<Player
+					autoplay
+					controls={false}
+					loop
+					speed={0.5}
+					src={"static/lottie-files/login-animation.json"}
+					style={{ height: "80%", width: "80%" }}
+				/>
+				<article className="max-w-xl text-center m-auto mt-4">
+					<h1 className="text-black text-lg leading-6">
+						<span className="font-bold">Class Planner - </span>Gerenciador de
+						horários de aula
+					</h1>
+					<p className="mt-2 text-gray ">
+						A autenticação é feita por meio do SUAP. Por mais que a matrícula
+						esteja ativa, apenas o público com permissão poderá acessar o
+						sistema, ou seja, apenas para funcionários da COADES/COAPAC e
+						professores.
+					</p>
+				</article>
+			</section>
+			<section className="w-full bg-background-color flex flex-col h-[100vh] justify-center items-center">
+				<form className="max-w-md m-auto p-4" onSubmit={handleSubmit(onSubmit)}>
+					<h2 className="text-2xl text-black font-bold">Entre com sua conta</h2>
+					<p className="text-base text-gray mb-4">
+						Utilize suas credenciais do SUAP para se autenticar
+					</p>
+
+					<Input
+						name="registration"
+						control={control}
+						label="Matrícula"
+						placeholder="Digite sua matrícula"
+						type="text"
+					/>
+					<Input
+						containerClassName="mt-2"
+						name="password"
+						control={control}
+						label="Senha"
+						placeholder="Digite sua senha"
+						type="password"
+					/>
+
+					<Button
+						isProcessing={isSubmitting}
+						className="w-full mt-6"
+						type="submit"
+					>
+						Entrar
+					</Button>
+				</form>
+			</section>
+		</div>
+	);
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,61 @@
+import React, { InputHTMLAttributes } from "react";
+import {
+	CustomFlowbiteTheme,
+	FlowbiteTheme,
+	Label,
+	LabelProps,
+	TextInput,
+	TextInputProps,
+	useTheme,
+} from "flowbite-react";
+import { Controller, Control } from "react-hook-form";
+
+interface IInputProps extends TextInputProps {
+	label?: string;
+	name: string;
+	control: Control<any>;
+	containerClassName?: HTMLDivElement["className"];
+}
+
+export default function Input({
+	label,
+	name,
+	control,
+	containerClassName,
+	...props
+}: IInputProps) {
+	return (
+		<div className={containerClassName}>
+			<Controller
+				name={name}
+				control={control}
+				render={({
+					field: { onBlur, onChange, ref },
+					formState,
+					fieldState: { error },
+				}) => (
+					<>
+						{label && (
+							<div className="mb-1 block">
+								<Label
+									color={error ? "failure" : props.color || "gray"}
+									htmlFor={props.id}
+									value={label}
+								/>
+							</div>
+						)}
+						<TextInput
+							color={error ? "failure" : "gray"}
+							helperText={error?.message}
+							name={name}
+							onBlur={onBlur}
+							onChange={onChange}
+							ref={ref}
+							{...props}
+						/>
+					</>
+				)}
+			/>
+		</div>
+	);
+}
