5c58afe315c548a2b3272cc70b7d8cd6ff674889
Bruna Maria <bruna.mariap@outlook.com>
2023-07-04 16:53:13 -0300

    Funcionalidade de excluir curso

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -9,10 +9,18 @@ import { api } from "@/services/api";
 import { GraduationCap, Plus, Users } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import DeleteModal from "@/components/DeleteModal";
 
 export default function Courses(): React.ReactNode {
 	const routes = useRouter();
 	const { courses, getAllCourses } = useGlobal();
+	const [openModal, setOpenModal] = useState<string | undefined>();
+
+	const deleteCourse = async (courseId: number) => {
+		await api.delete(`courses/${courseId}/`)
+		setOpenModal(undefined);
+		getAllCourses()
+	}
 
 	useEffect(() => {
 		getAllCourses();
@@ -31,7 +39,20 @@ export default function Courses(): React.ReactNode {
 
 			<section className="grid auto-rows-auto grid-cols-cardsGrid gap-5">
 				{courses.map(({ id, name, degree }) => (
-					<CourseCard key={id} courseId={id} courseGrade={degree} courseName={name} />
+					<CourseCard
+						key={id}
+						courseId={id}
+						courseGrade={degree}
+						courseName={name}
+					>
+						<DeleteModal key={id} type="course">
+							<Button key={id} color="sucess"
+								className="bg-success text-white"
+								onClick={() => deleteCourse(id)}>
+								Confirmar
+							</Button>
+						</DeleteModal>
+					</CourseCard>
 				))}
 			</section>
 		</div>
