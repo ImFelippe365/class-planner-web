fb6c1f6b534dc82a545efdd9cbc027023a051aa4
Bruna Maria <bruna.mariap@outlook.com>
2023-07-26 00:24:35 -0300

    Tentando exportar relatório mensal de professor

--- src/app/professores/[teacherId]/components/ExportTeacherReport.tsx
+++ src/app/professores/[teacherId]/components/ExportTeacherReport.tsx
@@ -19,6 +19,8 @@ const tw = createTw({
 				primary: "#007EA7",
 				gray: "#676767",
 				black: "#3D3D3D",
+				contrast: "#4A9E68",
+				canceled: "#C92A2A",
 			},
 			fontFamily: {
 				inter: ["Inter", "sans-serif"],
@@ -27,15 +29,61 @@ const tw = createTw({
 	},
 });
 
-const styles = StyleSheet.create({
-
-})
 
 export default function ExportTeacherReport({
 	teacher,
 	teacherMonthSchedules,
 	currentDate
 }: ExportTeacherReportProps) {
+
+	const styles = StyleSheet.create({
+		classContent: {
+			display: "flex",
+			flexDirection: "row",
+			flexWrap: "wrap",
+			gap: 12,
+		},
+		classesTaught: {
+			width: 100,
+			display: "flex",
+			flexDirection: "row",
+			flexWrap: "wrap",
+			fontSize: 12,
+			color: "#FFFFFF",
+			backgroundColor: "#4A9E68",
+			padding: 8,
+		},
+		futureClasses: {
+			width: 100,
+			display: "flex",
+			flexDirection: "row",
+			flexWrap: "wrap",
+			fontSize: 12,
+			color: "#FFFFFF",
+			backgroundColor: "#007EA7",
+			padding: 8,
+		},
+		canceledClasses: {
+			width: 100,
+			display: "flex",
+			flexDirection: "row",
+			flexWrap: "wrap",
+			fontSize: 12,
+			color: "#FFFFFF",
+			backgroundColor: "#C92A2A",
+			padding: 8,
+		},
+		substituteClasses: {
+			width: 100,
+			display: "flex",
+			flexDirection: "row",
+			flexWrap: "wrap",
+			fontSize: 12,
+			color: "#FFFFFF",
+			backgroundColor: "#FF8600",
+			padding: 8,
+		},
+	})
 
 	teacherMonthSchedules.sort((scheduele1, scheduele2) => {
 		const date1 = new Date(`${scheduele1.class_date} ${scheduele1.start_time}`);
@@ -59,37 +107,6 @@ export default function ExportTeacherReport({
 		return (new Date(`${schedule.class_date} ${schedule.start_time}`) < currentDate) && !schedule.canceled_class
 	})
 
-	interface ArrayObjects {
-		key?: Array<Schedule>;
-	}
-
-	let schedulesByClass = {};
-
-	/* const filterDisciplines = teacherMonthSchedules.map((schedule) => {
-
-		let classSchedules = []
-		let className = schedule.schedule_class.course.byname + schedule.schedule_class.shift[0] + schedule.schedule_class.reference_period
-		let disciplineName = formatDisciplineName(schedule.discipline.name)
-
-		const currentClass = []
-		if (className in schedulesByClass && !(schedulesByClass[className][disciplineName])) {
-
-			classSchedules.push({ [disciplineName]: [schedule] })
-			schedulesByClass[className] = [...classSchedules]
-		} else if (className in schedulesByClass && schedulesByClass[className][disciplineName]) {
-
-			classSchedules.push({ [disciplineName]: [...schedulesByClass[className][disciplineName], schedule] })
-			schedulesByClass[className] = [...classSchedules]
-		} else {
-
-			schedulesByClass = { [className]: { [disciplineName]: [schedule] } }
-		}
-
-
-		return schedulesByClass
-	}) */
-	//console.log(filterDisciplines)
-
 	const filteredDisciplines = teacherMonthSchedules.reduce((accumulator: any, schedule) => {
 
 		let className = schedule.schedule_class.course.byname + schedule.schedule_class.shift[0] + schedule.schedule_class.reference_period
@@ -103,6 +120,8 @@ export default function ExportTeacherReport({
 			return { ...accumulator, [className]: { [disciplineName]: [schedule] } }
 		}
 	}, {})
+
+	console.log(classesTaught)
 
 	console.log(filteredDisciplines)
 
@@ -133,48 +152,73 @@ export default function ExportTeacherReport({
 
 					<View style={tw("mt-4")}>
 						<Text style={tw("text-xl h-10")}>Aulas ministradas</Text>
-						<View style={tw("text-xl")} >
+						<View style={styles.classContent} >
 							{classesTaught.map((schedule) => (
-								<>
+								<View style={styles.classesTaught}>
 									<Text>
 										{(new Date(schedule.class_date)).toLocaleDateString()}
 									</Text>
 									<Text>
-										{schedule.start_time} - {schedule.end_time}: {formatDisciplineName(schedule.discipline.name)} - {schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}
+										{schedule.start_time} - {schedule.end_time}:
 									</Text>
-								</>
+									<Text style={tw("mt-3")}>{formatDisciplineName(schedule.discipline.name)}</Text>
+									<Text>{schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}</Text>
+								</View>
 							))}
 						</View>
 					</View>
 
 					<View style={tw("mt-4")}>
 						<Text style={tw("text-xl h-10")}>Aulas futuras</Text>
-						<View style={tw("text-xl")} >
+						<View style={styles.classContent} >
 							{futureClasses.map((schedule) => (
-								<>
+								<View style={styles.futureClasses}>
 									<Text>
 										{(new Date(schedule.class_date)).toLocaleDateString()}
 									</Text>
 									<Text>
-										{schedule.start_time} - {schedule.end_time}: {formatDisciplineName(schedule.discipline.name)} - {schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}
+										{schedule.start_time} - {schedule.end_time}:
 									</Text>
-								</>
+									<Text style={tw("mt-3")}>{formatDisciplineName(schedule.discipline.name)}</Text>
+									<Text>{schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}</Text>
+								</View>
 							))}
 						</View>
 					</View>
 
 					<View style={tw("mt-4")}>
-						<Text style={tw("text-xl")}>Aulas canceladas</Text>
-						<View style={tw("text-xl")} >
+						<Text style={tw("text-xl h-10")}>Aulas canceladas</Text>
+						<View style={styles.classContent} >
 							{canceledClasses.map((schedule) => (
-								<>
+								<View style={styles.canceledClasses}>
 									<Text>
 										{(new Date(schedule.class_date)).toLocaleDateString()}
 									</Text>
 									<Text>
-										{schedule.start_time} - {schedule.end_time}: {formatDisciplineName(schedule.discipline.name)} - {schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}
+										{schedule.start_time} - {schedule.end_time}:
 									</Text>
-								</>
+									<Text style={tw("mt-3")}>{formatDisciplineName(schedule.discipline.name)}</Text>
+									<Text>{schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}</Text>
+								</View>
+							))}
+						</View>
+					</View>
+
+					<View style={tw("mt-4")}>
+						<Text style={tw("text-xl h-10")}>Aulas que foram substituídas</Text>
+						<View style={styles.classContent} >
+							{substituteClasses.map((schedule) => (
+								<View style={styles.substituteClasses}>
+									<Text>
+										{(new Date(schedule.class_date)).toLocaleDateString()}
+									</Text>
+									<Text>
+										{schedule.start_time} - {schedule.end_time}:
+									</Text>
+									<Text style={tw("mt-3")}>{formatDisciplineName(schedule.discipline.name)}</Text>
+									<Text>{schedule.schedule_class.course.byname}{schedule.schedule_class.shift[0]}{schedule.schedule_class.reference_period}</Text>
+									<Text>Substituída por {schedule.class_to_replace.teacher.name}</Text>
+								</View>
 							))}
 						</View>
 					</View>
@@ -200,17 +244,23 @@ export default function ExportTeacherReport({
 						<View style={tw("text-xl")} >
 
 
-							{/* {Object.entries(filteredDisciplines).map(([key, value]) => (
+							{Object.entries(filteredDisciplines).map(([className, disciplineName]) => (
 								<View>
-									<Text>{key}</Text>
-									<Text>Total de aulas previstas</Text>
-									{value.map((schedule: Schedule) => (
-										<View>
-											<Text>{schedule.start_time}</Text>
-										</View>
+									<Text>{className}</Text>
+									{/* <Text>{disciplineName}</Text> */}
+									{Object.entries(disciplineName).map(({disciplineNamekey, scheduleArray }) => (
+										<>
+											<Text>{disciplineNamekey}</Text>
+											{/* {scheduleArray.map((schedule: Schedule) => (
+												<>
+													<Text>{schedule.quantity}</Text>
+												</>
+											))} */}
+										</>
 									))}
+
 								</View>
-							))} */}
+							))}
 
 							{/* <Text>
 										{(new Date(schedule.class_date)).toLocaleDateString()}
